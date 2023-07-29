3fa89d8b48a1c424914b178cb89cd44d6a453c6e
Kanny <kanny.ghafour87@gmail.com>
2023-07-29 10:49:51 -0500

    lab completed

--- src/index.js
+++ src/index.js
@@ -1,44 +1,51 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HI', 'color: firebrick');
+
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
 
 const fetchCall = () => {
-    const imgs = document.querySelector("#dog-image-container")
+    const imgs = document.querySelector("#dog-image-container");
 
     const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
-      fetch(imgUrl)
-      .then((res)=> res.json())
-      .then(data=>{
+    fetch(imgUrl)
+        .then((res) => res.json())
+        .then(data => {
+            data.message.map(element => {
 
-        data.message.map(element => {
-            console.log(element)
-            imgs.innerHTML += `<img src=${element}>`
-            
+                imgs.innerHTML += `<img src=${element}>`;
+            });
         });
-      })
 
-
-      const breedUrl = "https://dog.ceo/api/breeds/list/all";
-      const optionA = document.querySelector('#breed-dropdown option[value="a"]');
-      const optionB = document.querySelector('#breed-dropdown option[value="b"]');
-      const optionC = document.querySelector('#breed-dropdown option[value="c"]');
-      const optionD = document.querySelector('#breed-dropdown option[value="d"]');
-      const ul = document.querySelector('#dog-breeds');
-      fetch(breedUrl)
-      .then((res)=> res.json())
-      .then(data=>{
-        Object.keys(data.message).forEach(element => {
-            ul.innerHTML += `<li>${element}</li>`  
-        });
-            const li = document.getElementsByTagName('li')
-            for(let i=0; i<=li.length; i++){
-                const el = li[i];
-                console.log(el)
-            el.addEventListener('click', ()=>{
-                el.style.color ='red'
-            })
-        }
-
+        const breedUrl = "https://dog.ceo/api/breeds/list/all";
+        const ul = document.querySelector('#dog-breeds');
+        const dropdown = document.querySelector('#breed-dropdown');
         
-      })}
+        fetch(breedUrl)
+            .then((res) => res.json())
+            .then(data => {
+                const breedList = Object.keys(data.message);
+                const filterBreedsByLetter = (letter) => {
+                    ul.innerHTML = ''; 
+                    breedList.forEach(breed => {
+                        if (letter === 'all' || breed.startsWith(letter)) {
+                            ul.innerHTML += `<li>${breed}</li>`;
+                        }
+                    });
+                    const li = document.getElementsByTagName('li');
+                    for (let i = 0; i < li.length; i++) {
+                        const el = li[i];
+                        el.addEventListener('click', () => {
+                            el.style.color = 'red';
+                        });
+                    }
+                };
+
+                filterBreedsByLetter('all');
+                dropdown.addEventListener('change', () => {
+                    const selectedLetter = dropdown.value;
+                    filterBreedsByLetter(selectedLetter);
+                });
+            });
+        }  
 
 document.addEventListener('DOMContentLoaded', fetchCall);
+
